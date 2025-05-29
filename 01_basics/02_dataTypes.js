04f3c0d345078aa6f6f2a70615cd4c3d753b24b5
Harish Verma <hari734000@gmail.com>
2025-05-29 04:34:51 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+let name = "Hrish verma"; // String
+let age = 23; //number
+let isLogedIn = true; //boolean
+let state; //undefined
+let insta = null; //standlone value
+
+//symbol => unique value
+
+// object
+
+/* 
+If we want to check the value of any declared variable then we use console.log(typeof variable_name);
+*/
+
+console.log(typeof "Harish");
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+

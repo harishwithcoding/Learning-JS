a34005823b501cbd2b93bdc7497dee55b461e426
Harish Verma <hari734000@gmail.com>
2025-05-29 06:43:38 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score =33;
+let score2 = "23";
+
+let conertInNumber = Number(score2);
+
+// console.log(typeof score);
+// console.log(typeof score2);
+// console.log(typeof conertInNumber);
+
+// let userLoggedIn = 1;
+// let userLoggedIn = "1";
+// let userLoggedIn = "";
+let userLoggedIn = "Harish";
+
+let convertInBoolean = Boolean(userLoggedIn);
+
+console.log(convertInBoolean);
+console.log(typeof convertInBoolean);
+
+// 1 => true, 0 => false
+// "" => false
+// "Harish" => true
+
+

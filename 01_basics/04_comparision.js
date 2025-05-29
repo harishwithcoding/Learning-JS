dd3577f54a7d92427c88854935c3a415d8373ea1
Harish Verma <hari734000@gmail.com>
2025-05-29 07:32:56 +0000

    comparision operators

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,7 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log(2 === "2"); // This is false beacause first 2 is number and the second number is string

c64f32e43bd3450862fa105302bb0e974352cdc6
Harish Verma <hari734000@gmail.com>
2025-05-30 05:59:35 +0000

    Another array's methods

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,27 @@
+const marvel_heros = ["Thor", "Ironman", "Spideram"];
+const dc_heros = ["Superman", "Flesh", "Batman"];
+
+// marvel_heros.push(dc_heros);
+// console.log(allHeros[3][1]); // now this is show Flesh as a result
+
+
+// const allHeros = marvel_heros.concat(dc_heros);
+// console.log(allHeros);
+
+// const all_heros = [...marvel_heros, ...dc_heros];
+// console.log(all_heros);
+
+// const new_num = [1,2,3,[4,5], 7,[6,7,[3,6]]];
+// const new1 = new_num.flat(Infinity);
+
+// console.log(new1);
+
+// console.log(Array.isArray("Harish"));
+// console.log(Array.from("Harish"));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+

ff9e98a7b2fedc74947e99131cf01326b16b943e
Harish Verma <hari734000@gmail.com>
2025-05-30 07:21:53 +0000

    some another methods of object

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,35 @@
+// singleton
+
+// const tindderUser = new Object();
+
+
+const tindderUser = {};
+tindderUser.id = "123abc";
+tindderUser.name = "harry";
+tindderUser.loggedIn = false;
+console.log(tindderUser);
+
+
+const regularUser = {
+    email: "User1@gmial.com",
+    full_name: {
+        userFullName:{
+            firstName: "Harish",
+            lastName: "Verma",
+        }
+    }
+}
+
+// console.log(regularUser.full_name.userFullName.firstName);
+
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "c", 4: "d"};
+
+// const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+console.log(Object.keys(tindderUser));
+console.log(Object.values(tindderUser));
+

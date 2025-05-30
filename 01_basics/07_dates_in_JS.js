12f42c57b4bb0a34f12c4026e3530bae0403e2cc
Harish Verma <hari734000@gmail.com>
2025-05-30 04:26:40 +0000

    dates in js

---
+++ 01_basics/07_dates_in_JS.js
@@ -0,0 +1,21 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let dateCreate = new Date(2025, 0, 30);
+// console.log(dateCreate);
+// console.log(dateCreate.toDateString());
+
+// let dateCreate = new Date(2025, 0, 30, 5, 30); // This is use for printing time with date.
+// console.log(dateCreate.toLocaleString());
+
+// let dateCreate = new Date("2025-05-30");
+// console.log(dateCreate.toDateString());
+
+let dateCreate = new Date("05-30-2025");
+console.log(dateCreate.toLocaleString());
+
+

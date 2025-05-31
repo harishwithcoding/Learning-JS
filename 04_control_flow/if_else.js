5ed13c692c83aea717a1631431d6f63fb92c74fa
Harish Verma <hari734000@gmail.com>
2025-05-31 09:29:30 +0000

    learn if else

---
+++ 04_control_flow/if_else.js
@@ -0,0 +1,69 @@
+// if
+// const user = true;
+
+// if (user) {
+//     console.log("user logged in");
+// }
+
+
+// if (2==="2") {
+//     console.log("user logged in"); // not executed because === opertor check also type of the value
+// }
+
+
+// if else
+// let temp = 49;
+// if(temp < 40){
+//     console.log(`themp. is less then 40`);
+// }
+// else{
+//     console.log(`temp. is greater then 40`);
+    
+// }
+
+// if(true) console.log("test"); // this is not a good prectice.
+// if(true) console.log("test"), console.log("test2"); // this is not a good prectice.
+
+
+// if else if
+// let cost = 1000;
+// if(cost < 200){
+//     console.log("less then 200");
+// } else if(cost < 500){
+//     console.log("less then 500");
+// } else{
+//     console.log("grater then 500");
+// };
+
+
+// Double conditions
+
+// And(&&) operator allow you if your all conditions are true, if any-one if false then it will not executed.
+// let userLoggedIn = true;
+// // let userLoggedIn = false;
+// let debitCard = true;
+
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy car");
+// } else{
+//     console.log("Not allowed");
+// }
+
+// Or(||) operator allow you if at-list one condition is true. 
+
+let userLoggedIn = true;
+// let userLoggedIn = false;
+let debitCard = false;
+
+if(userLoggedIn || debitCard){
+    console.log("Allow to buy car");
+} else{
+    console.log("Not allowed");
+} 
+
+
+
+
+
+
+

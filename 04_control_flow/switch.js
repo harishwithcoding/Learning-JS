b71571204d923d6420156aef84a05ca996a6d638
Harish Verma <hari734000@gmail.com>
2025-05-31 09:41:40 +0000

    switch case statement learn

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,45 @@
+const month = 6;
+
+
+// In switch case break use are mandatory because if we not use break statement then after that all the code are execute where it will find the break statement it will break and came out form loop
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("febuary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        // break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("september");
+        break;
+    case 10:
+        console.log("october");
+        break;
+    case 11:
+        console.log("november");
+        break;
+    case 12:
+        console.log("december");
+        break;
+    default:
+        console.log("Not allowed");
+        break;
+}

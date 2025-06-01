681040165cf66b8d832902077a51bc9036007f41
Harish Verma <hari734000@gmail.com>
2025-06-01 06:16:58 +0000

    for in loop in array and object

---
+++ 05_iterations/four.js
@@ -0,0 +1,21 @@
+const myObj = {
+    js: "Javascript",
+    cpp: "C++",
+    rb: "Ruby",
+    py: "Python",
+};
+
+for (const key in myObj) {
+//    console.log(key);
+//    console.log(myObj[key]);
+//    console.log(`${key} is shortcut for ${myObj[key]}`);
+};
+
+const programming = ["js", "cpp", "ry","py"];
+
+for (const key in programming) {
+    // console.log(key);
+    console.log(programming[key]);
+    
+    
+}
